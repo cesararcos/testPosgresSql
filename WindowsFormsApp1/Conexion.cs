c55dcf86b39ed8eca33386c1d7a4d365a3bd4b91
Reack <ceesitar93@gmail.com>
2021-03-05 01:49:41 -0500

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp1/Conexion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace WindowsFormsApp1
+{
+    public class Conexion
+    {
+        NpgsqlConnection conn = new NpgsqlConnection("Server = localhost; User Id= postgres; Password = cesar; Database = BDPruebaTecnica");
+
+        public void Conectar()
+        {
+            //conn.Open();
+            MessageBox.Show("Listo");
+
+
+        }
+
+        public void Insertar(string fecha, int valor, string usuario, Boolean habilitado, int id)
+        {
+            //fecha = DateTime.Now;
+
+            string query = "Insert into \"arl\" values (" + id + "," + valor + ",'" + usuario + "','" + fecha + "','" + habilitado + "')";
+
+            conn.Open();
+            NpgsqlCommand ejecutor = new NpgsqlCommand(query, conn);
+            ejecutor.ExecuteNonQuery();
+            MessageBox.Show("hecho ya guardo");
+            conn.Close();
+        }
+
+        public DataTable Buscar()
+        {
+            string query = "select * from \"contratoslaborales\" ORDER BY idcontrato DESC LIMIT 1";
+            NpgsqlCommand conector = new NpgsqlCommand(query, conn);
+            NpgsqlDataAdapter datos = new NpgsqlDataAdapter(conector);
+            DataTable tabla = new DataTable();
+            datos.Fill(tabla);
+
+            return tabla;
+
+            //string query = "select * from \"contratoslaborales\" where \"numerodocumento\" = '31378091' ";
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Conexion conectandose = new Conexion();
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            conectandose.Insertar("2021-03-04", 40, "cesar", true, 4);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = conectandose.Buscar();
+            var id = dataGridView1.Rows[0].Cells[0].Value;
+        }
+    }
+}
